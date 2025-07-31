df65892bdf06ef2ef3186d636206ed4c97ecde6a
Alexandre <alexandre.rizk@free.fr>
2025-07-31 13:30:51 +0200

    First commit

---
+++ App/app.py
@@ -0,0 +1,185 @@
+import gradio as gr
+
+from functions import new_question, check_answer, toggle_mode_libre, toggle_switch_base_dict, reset_score, toggle_no_mistake
+from data import hirukana_dict, question_hirukana
+
+
+with gr.Blocks(css="""
+    #quiz-container {
+        /*max-width: 1000px;*/
+        max-width: 60%;
+        width: 100%;
+        padding: 5vw;
+        margin: auto; 
+        padding: 30px; 
+        background: #f9f9f9; 
+        border-radius: 12px; 
+        box-shadow: 0 6px 18px rgba(0,0,0,0.12);
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+    #title {
+        text-align: center;
+        font-size: 2.8em;     
+        font-weight: 700;
+        margin-bottom: 25px;
+        color: #2c3e50;
+    }
+    #settings_row {
+        max-width: 60%;
+        width: 100%;
+        padding: 5vw;
+        margin: auto; 
+        padding: 30px; 
+        display: flex;
+        justify-content: center;
+        margin-bottom: 25px;
+    }
+    #question_markdown {
+        text-align: center;
+        font-size: 3em;       
+        margin-bottom: 20px;
+        color: #34495e;
+        min-height: 110px;
+    }
+    #options_radio {
+        display: flex !important;
+        flex-direction: column !important;
+        gap: 12px;
+    }
+    #options_radio > label {
+        font-size: 1.4em;      
+        margin-bottom: 12px;
+        cursor: pointer;
+    }
+    #result_box {
+        text-align: center;
+        font-weight: 700;
+        margin-top: 18px;
+        min-height: 40px;
+        font-size: 1.3em;
+    }
+    #score_box {
+        text-align: center;
+        margin-top: 15px;
+        font-size: 1.3em;
+        color: #34495e;
+    }
+    #max_score {
+        text-align: center;
+        margin-top: 15px;
+        font-size: 1.3em;
+        color: #34495e;
+    }
+    #reset_score_btn {
+        width: 120px;        
+        height: 30px;
+        text-align: center;
+        /*margin: 0 auto;*/  /*Not used, can be used to center the reset button*/
+        margin-left: auto;
+        margin-right: 0;       
+        background-color: #FF4136;
+        color: white;
+    }
+    #buttons_row {
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+        margin-top: 30px;
+    }
+    #buttons_row button {
+        width: 140px;        
+        height: 50px;          
+        font-weight: 700;
+        font-size: 1.2em;      
+        border-radius: 12px;
+        padding: 0;
+        cursor: pointer;
+        border: none;
+        background-color: #2980b9;
+        color: white;
+        transition: background-color 0.3s ease;
+    }
+    #buttons_row button:disabled {
+        background-color: #95a5a6;
+        cursor: not-allowed;
+    }
+    #buttons_row button:hover:not(:disabled) {
+        background-color: #3498db;
+    }
+""") as interface:
+    
+    # Title
+    gr.Markdown("## 🏯 Japanese Quiz 🏯", elem_id="title")
+
+    # Settings
+    with gr.Row(elem_id="settings_row"):
+        mode_libre = gr.Checkbox(label="📝 Type your answer", value=False)
+        mode_full_words = gr.Checkbox(label="🔡 Full words", value=False)
+        mode_switch_language = gr.Checkbox(label="🔁 Switch language", value=False)
+        mode_no_mistake = gr.Checkbox(label="❌✍️ No mistake", value=False)
+
+    # Quiz
+    with gr.Row(elem_id="quiz-container"):
+
+        # Question and user input
+        question_text = gr.Markdown("", elem_id="question_markdown")
+        answer_options = gr.Radio(choices=[], label="", elem_id="options_radio")
+        text_input = gr.Textbox(placeholder="Write your answer: ", label="", visible=False, interactive=True)
+        
+        # Results and score
+        with gr.Column():
+            reset_score_btn = gr.Button("Reset Score", elem_id="reset_score_btn")
+            result = gr.Markdown("", elem_id="result_box")
+            score_box = gr.Markdown("", elem_id="score_box")
+            max_score = gr.Markdown(f"**🏆 Max score: 0**", visible=False, elem_id="max_score")
+
+        # Validate and next button    
+        with gr.Row(elem_id="buttons_row"):
+                check_btn = gr.Button("☑️ Validate", elem_id="check_btn")
+                next_btn = gr.Button("➡️ Next", elem_id="next_btn")
+
+        # Initial state
+        next_btn.disabled = True
+        dict_used = gr.State(hirukana_dict)
+        question_used = gr.State(question_hirukana)
+        current_question = gr.State({"question": "", "all-answers": [], "right_answer": ""})
+        score = gr.State({"correct": 0, "total": 0, "max_score": 0})
+
+        # Toggle different setting modes
+        mode_libre.change(fn=toggle_mode_libre, 
+                          inputs=mode_libre,
+                          outputs=[result, text_input, answer_options])
+        
+        mode_switch_language.change(fn=toggle_switch_base_dict,
+                               inputs=[mode_switch_language, mode_full_words, current_question, score],
+                               outputs=[dict_used, question_used, question_text, answer_options, text_input, result, score_box, check_btn, next_btn, result])
+        
+        mode_full_words.change(fn=toggle_switch_base_dict,
+                               inputs=[mode_switch_language, mode_full_words, current_question, score],
+                               outputs=[dict_used, question_used, question_text, answer_options, text_input, result, score_box, check_btn, next_btn, result])
+        
+        mode_no_mistake.change(fn=toggle_no_mistake,
+                               inputs=[mode_no_mistake, score],
+                               outputs=[score_box, max_score])
+        
+        # Buttons click
+        reset_score_btn.click(fn=reset_score,
+                              inputs=score,
+                              outputs=score_box)
+                
+        check_btn.click(fn=check_answer, 
+                        inputs=[answer_options, text_input, mode_libre, mode_no_mistake, current_question, score], 
+                        outputs=[question_text, answer_options, result, score_box, check_btn, next_btn, result, max_score])
+        
+        next_btn.click(fn=new_question, 
+                       inputs=[dict_used, question_used, current_question, score], 
+                       outputs=[question_text, answer_options, text_input, result, score_box, check_btn, next_btn, result])
+
+        # Interface setup
+        interface.load(fn=new_question, 
+                       inputs=[dict_used, question_used, current_question, score], 
+                       outputs=[question_text, answer_options, text_input, result, score_box, check_btn, next_btn, result])
+
+
+if __name__ == "__main__":
+    interface.launch()
---
+++ App/functions.py
@@ -0,0 +1,243 @@
+import gradio as gr
+import random
+
+from data import hirukana_dict, reverse_hirukana_dict, translation_dict, reverse_translation_dict, question_hirukana, question_reverse_hirukana, question_translation, question_reverse_translation
+
+
+def reset_score(score):
+
+    """
+    Reset the score displayed
+
+    Args:
+        score (dict): 
+
+    Returns:
+        score (str): string to display with the current score
+    """
+
+    score['correct'] = 0
+    score['total'] = 0
+
+    return f"Score: {score['correct']} / {score['total']}"
+
+
+def make_question(base_dict, question_used, num_choice=4):
+
+    """
+    Select a random character or word and its traduction along with several wrong traductions to create a question
+
+    Args:
+        base_dict (dict): The dict of character/word and traductions to use for questions
+        question_used (str): The base text of the question
+        num_choice (int): The number of choice for multiple choice mode
+
+    Returns:
+        question (str): The full text of the question to show on the interface
+        all_answers (list): The list of all suggested answers
+        right_answer (str): The correct answer
+    """
+
+    character = random.choice(list(base_dict.keys()))
+    question = question_used + "<br> **" + character + "**"
+    right_answer = base_dict[character]
+    other_keys = {k: v for k, v in base_dict.items() if k!= character}
+
+    all_answers = [right_answer]
+    all_answers += random.sample(list(other_keys.values()), num_choice-1)
+    random.shuffle(all_answers)
+
+    return question, all_answers, right_answer
+
+
+def new_question(base_dict, question_used, current_question, score):
+
+    """
+    Create a new question and return a state for the interface
+
+    Args:
+        base_dict (dict): The dict of character/word and traductions to use for questions
+        question_used (str): The base text of the question
+        current_question (dict): 
+        score (dict): 
+
+    Returns:
+        tuple:
+            - question text
+            - empty string for text input
+            - empty string for result
+            - score string
+            - validate button with interactive state turned on (can be clicked)
+            - next button with interactive state turned off (can't be clicked)
+            - result message turned to non visible
+
+    """
+
+    question, all_answers, right_answer = make_question(base_dict, question_used)
+    current_question["question"] = question
+    current_question["all_answers"] = all_answers
+    current_question["right_answer"] = right_answer
+
+    return (question, 
+            gr.update(choices=all_answers, value=None),
+            "",  # vide text_input
+            "",  # vide result
+            f"Score: {score['correct']} / {score['total']}",
+            gr.Button(interactive=True),  # Validate button is activate when a new question is create
+            gr.Button(interactive=False),    # Next button is deactivate and will be turned on only when an answer is chosen/written 
+            gr.update(visible=False)  # Result message is hidden at the start
+    )
+
+
+def check_answer(selected_option, text_answer, mode_libre, mode_no_mistake, current_question, score):
+
+    """
+    Checks whether the user's answer is correct based on the quiz mode
+
+    Args:
+        selected_option (str): The selected radio button answer (in multiple-choice mode)
+        text_answer (str): The answer typed by the user (in free-answer mode)
+        mode_libre (bool): Whether free-answer mode is active
+        mode_no_mistake (bool): Whether the no-mistake mode is active (score reset when a mistake is made)
+        current_question (dict): 
+        score (dict):
+
+    Returns:
+        tuple:
+            - question text
+            - all choice of answers updated with the current question and associated answers
+            - result string message
+            - score string
+            - validate button with interactive state
+            - next button with interactive state
+            - result message with visibility state
+            - max score string
+    """
+
+    if not (selected_option or text_answer):
+
+        return (current_question["question"], 
+                gr.update(choices=current_question["all_answers"]), 
+                "Select an answer",
+                f"Score: {score['correct']} / {score['total']}",
+                gr.Button(interactive=True),  # Valider reste activé
+                gr.Button(interactive=False),    # Suivant désactivé
+                gr.update(visible=False),
+                f"**🏆 Max score: {score['max_score']}**"
+
+        )
+    
+    score["total"] += 1
+    
+    if mode_libre:
+        right_answers = current_question['right_answer'].split('/')
+        correct = False
+        for right_answer in right_answers:
+            correct = (text_answer.strip().lower() == right_answer.strip().lower()) or (text_answer.split('(')[0].strip().lower() == right_answer.split('(')[0].strip().lower())
+    else:
+        correct = selected_option == current_question["right_answer"]
+
+    if correct:
+        score["correct"] += 1
+        message = "✅ Correct !"
+
+        if mode_no_mistake:
+            if score['correct'] > score['max_score']:
+                score['max_score'] = score['correct'] 
+    
+    else:
+        message = f"❌ Wrong answer, it was : **{current_question['right_answer']}**"
+
+        if mode_no_mistake:
+            if score['correct'] > score['max_score']:
+                score['max_score'] = score['correct']
+            reset_score(score)
+            message += "<br> Your score was reset"
+            
+
+    return (current_question["question"], 
+            gr.update(choices=current_question["all_answers"]), 
+            message,
+            f"Score: {score['correct']} / {score['total']}",
+            gr.Button(interactive=False),   # Désactiver Valider
+            gr.Button(interactive=True),   # Activer Suivant
+            gr.update(visible=True),
+            f"**🏆 Max score: {score['max_score']}**"
+)
+
+
+def toggle_mode_libre(mode):
+
+    """
+    Function called when free text mode is toggled on
+
+    Args:
+        mode (bool): state of the free text mode toggle
+
+    Returns:
+        result (str): empty string for result (reset result)
+        text_input: update the visibility state of text_input (free text) depedending on the mode
+        answer_options: update the visibility state of answer_options (multiple choice box) depedending on the mode
+    """
+
+    if mode:
+        return "", gr.update(visible=True), gr.update(visible=False)
+    else:
+        return "", gr.update(visible=False), gr.update(visible=True)
+    
+
+def toggle_switch_base_dict(mode_lang, mode_words, current_question, score):
+
+    """
+    Function called when the translation dict to use is changed (either the mode is change from characters to words or the language is switched)
+
+    Args:
+        mode_lang (bool): state of the switch language mode, True when the mode is toggled on and language is switched to fr->jp
+        mode_words (bool): state of the full words mode, True when the mode is toggled on and full words need to be translated
+        current_question (dict): 
+        score (dict):
+
+    Returns:
+        base_dict (dict):  The dict of character/word and traductions to use for questions
+        base_question (str): The base text of the question
+        new_question outputs (check new_question function documentation)
+    """
+
+    if mode_lang:
+        if mode_words:
+            base_dict = reverse_translation_dict
+            base_question = question_reverse_translation
+        else:
+            base_dict = reverse_hirukana_dict
+            base_question = question_reverse_hirukana
+    else:
+        if mode_words:
+            base_dict = translation_dict
+            base_question = question_translation
+        else:
+            base_dict = hirukana_dict
+            base_question = question_hirukana
+
+    return base_dict, base_question, *new_question(base_dict, base_question, current_question, score)  # a new question is generated when the mode is changed
+            
+
+def toggle_no_mistake(mode_no_mistake, score):
+
+    """
+    Function called when the no mistake mode is toggled on
+
+    Args:
+        mode_no_mistake (bool): state of the no mistake mode, True when the mode is toggled on
+        score (dict): 
+
+    Returns:
+        new_score (str): score text that is reset automatically when the no mistake mode is toggled on
+        max_score: update the visibility of the max score display (visible only in no mistake mode)
+    """
+
+    new_score = reset_score(score)
+
+    if mode_no_mistake:
+        return new_score, gr.update(visible=True)
+    else:
+        return new_score, gr.update(visible=False)
